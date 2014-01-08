c821a6a11199ddf2eeb478862c3f9ea8e12c4151
wanax <mxiaochi@gmail.com>
2014-01-08 10:58:07 +0800

    Initial Commit

---
+++ GooGuu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GooGuu
+//
+//  Created by Xcode on 14-1-8.
+//  Copyright (c) 2014年 Xcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
